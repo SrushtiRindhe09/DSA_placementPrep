4a1a70370ad7520c601657c3405f9eba0eb1e63b
SrushtiRindhe09 <rindhesrushti2006@gmail.com>
2026-01-26 22:48:58 +0530

    demo cpp code

---
+++ practiceCode/demo.cpp
@@ -0,0 +1,37 @@
+/*#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+    cout << "Enter first number: ";
+    cin >> a;
+     cout << "Enter second number: ";
+    cin >> b;
+    c = a + b;
+    cout << "addition is: "<< c; 
+}*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    c = a * b;
+
+    cout << "multiplication is: " << c;
+
+    return 0;
+}
